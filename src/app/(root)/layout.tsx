625a7ddd8fcf84977031c8962fa3fe31b0769f41
Gagandeep <igagandeep95@gmail.com>
2025-05-08 20:04:10 -0400

    implement home page

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,8 +1,20 @@
 import {ReactNode} from 'react'
+import Link from "next/link";
+import Image from "next/image";
 
 const RootLayout = ({children}: { children: ReactNode }) => {
     return (
-        <div>{children}</div>
+        <div className="root-layout">
+            <nav>
+                <Link href="/" className="flex items-center gap-2">
+                    <Image alt="logo" src="/logo.png" height={32} width={38}/>
+                    <h2 className="text-primary-100">MockMate</h2>
+                </Link>
+            </nav>
+
+            {children}
+
+        </div>
     )
 }
 export default RootLayout
