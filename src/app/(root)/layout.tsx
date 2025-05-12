466a7990db012a2d673b27a11729cfaa826cdca9
Gagandeep <igagandeep95@gmail.com>
2025-05-12 17:46:12 -0400

    build interview session

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -13,8 +13,8 @@ const RootLayout = async ({ children }: { children: ReactNode }) => {
         <div className="root-layout">
             <nav>
                 <Link href="/"  className="flex items-center gap-2" >
-                    <Image src="/logo.svg" alt="Logo" width={38} height={32} />
-                    <h2 className="text-primary-100">PrepWise</h2>
+                    <Image src="/logo.png" alt="Logo" width={38} height={32} />
+                    <h2 className="text-primary-100">Mock Mate</h2>
                 </Link>
             </nav>
 
